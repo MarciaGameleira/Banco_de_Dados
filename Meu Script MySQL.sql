5adb23e2beb84da1b82bd1f8a105958022a8c4cd
MarciaGameleira <gameleira.mag@gmail.com>
2021-03-09 04:10:29 -0300

    .

--- Meu Script MySQL.sql
+++ Meu Script MySQL.sql
@@ -214,25 +214,75 @@ SELECT * FROM CLIENTE; /*Observe que não houve exclusão de nenhum registro poi
 +--------+------+------------------+-----------+-----------+-----------------------------------------------+*/
 
 CREATE DATABASE COMERCIO;
+USE COMERCIO
 
+CREATE TABLE CLIENTE(
+	IDCLIENTE INT PRIMARY KEY AUTO_INCREMENT,/*primary key serve para identificar a coluna que possuidora da chave primária. AUTO_INCREMENTE é uma função de inserção automatica da chave primária.*/
+	NOME VARCHAR(30) NOT NULL,
+	SEXO ENUM('M','F') NOT NULL,/*A função ENUM só existe no MySQL.*/
+	EMAIL VARCHAR(50) UNIQUE,/*A função unique não aceita outro campo dentro da coluna que sejam iguais.*/
+	CPF VARCHAR(15) UNIQUE
+);
 
+CREATE TABLE ENDERECO(
+	IDENDERECO INT PRIMARY KEY AUTO_INCREMENT, 
+	RUA VARCHAR(30) NOT NULL,
+	BAIRRO VARCHAR(30) NOT NULL,
+	CIDADE VARCHAR(30) NOT NULL,
+	ESTADO CHAR(2) NOT NULL,
+	ID_CLIENTE INT UNIQUE,
+	FOREIGN KEY(ID_CLIENTE) REFERENCES CLIENTE(IDCLIENTE)/*Criando a chave estrangeira.*/
+);
+CREATE TABLE TELEFONE(
+	IDTELEFONE INT PRIMARY KEY AUTO_INCREMENT, 
+	TIPO ENUM('RES','COM','CEL') NOT NULL,
+	NUMERO VARCHAR(10) NOT NULL,
+	ID_CLIENTE INT,
+	FOREIGN KEY(ID_CLIENTE)
+	REFERENCES CLIENTE(IDCLIENTE)
+);
 
+INSERT INTO CLIENTE VALUES(NULL,'JOAO','M','JOAOA@IG.COM','76567587887');
+INSERT INTO CLIENTE VALUES(NULL,'CARLOS','M','CARLOSA@IG.COM','5464553466');
+INSERT INTO CLIENTE VALUES(NULL,'ANA','F','ANA@IG.COM','456545678');
+INSERT INTO CLIENTE VALUES(NULL,'CLARA','F',NULL,'5687766856');
+INSERT INTO CLIENTE VALUES(NULL,'JORGE','M','JORGE@IG.COM','8756547688');
+INSERT INTO CLIENTE VALUES(NULL,'CELIA','M','JCELIA@IG.COM','5767876889');
 
+INSERT INTO ENDERECO VALUES(NULL,'RUA ANTONIO SA','CENTRO','B. HORIZONTE','MG',4);
+INSERT INTO ENDERECO VALUES(NULL,'RUA CAPITAO HERMES','CENTRO','RIO DE JANEIRO','RJ',1);
+INSERT INTO ENDERECO VALUES(NULL,'RUA PRES VARGAS','JARDINS','SAO PAULO','SP',3);
+INSERT INTO ENDERECO VALUES(NULL,'RUA ALFANDEGA','ESTACIO','RIO DE JANEIRO','RJ',2);
+INSERT INTO ENDERECO VALUES(NULL,'RUA DO OUVIDOR','FLAMENGO','RIO DE JANEIRO','RJ',6);
+INSERT INTO ENDERECO VALUES(NULL,'RUA URUGUAIANA','CENTRO','VITORIA','ES',5);
 
+INSERT INTO TELEFONE VALUES(NULL,'CEL','78458743',5);
+INSERT INTO TELEFONE VALUES(NULL,'RES','56576876',5);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','87866896',1);
+INSERT INTO TELEFONE VALUES(NULL,'COM','54768899',2);
+INSERT INTO TELEFONE VALUES(NULL,'RES','99667587',1);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','78989765',3);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','99766676',3);
+INSERT INTO TELEFONE VALUES(NULL,'COM','66687899',1);
+INSERT INTO TELEFONE VALUES(NULL,'RES','89986668',5);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','88687909',2);
 
+select nome, email, cidade from cliente, endereco where idcliente = id_cliente;/*Fazendo a junção das tabelas.*/
 
+select nome, cidade from cliente, endereco where idcliente = id_cliente and sexo = 'f';
 
+INSERT INTO ENDERECO VALUES(NULL,'RUA ANTONIO SA','CENTRO','B. HORIZONTE','MG',15);
 
+SELECT NOME, SEXO, BAIRRO, CIDADE FROM CLIENTE INNER JOIN ENDERECO ON IDCLIENTE = ID_CLIENTE;/*Junção de tabelas: INNER JOIN com o ON.*/
 
+select nome, sexo, email, tipo, numero from cliente inner join telefone on idcliente = id_cliente;
 
-
-
-
-
-
-
-
-
+/*Inner Join de mais de duas tabelas com técnica de apontamento de tabela em cada coluna informada na query.*/
+select c.nome, c.sexo, e.bairro, e.cidade, t.tipo, t.numero from cliente c
+inner join endereco e
+on c.idcliente = e.id_cliente
+inner join telefone t
+on c.idcliente = t.id_cliente;
 
 
 
